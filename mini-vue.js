4a99ea6cb6b7b02c6ef3fbe7988300991a0a220c
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-12-11 12:41:05 -0300

    `mini-vue` file for component creation

---
+++ mini-vue.js
@@ -0,0 +1,23 @@
+import { reactive, watchEffect } from "./reactivity";
+
+export function createApp({ data, template, methods }) {
+  const state = reactive(data());
+
+  const app = document.querySelector('#app');
+  app.innerHTML = template;
+
+  watchEffect(() => {
+    app.querySelectorAll('[data-bind]').forEach((node) => {
+      const key = node.getAttribute('data-bind');
+      node.textContent = state[key];  
+    });
+  });
+
+  Object.entries(methods).forEach(([key, fn]) => {
+    app.querySelectorAll(`[data-methods="${key}"]`).forEach((node) => {
+      node.addEventListener('click', () => fn.call(state));     
+    });
+  });
+
+  return { state };
+}
